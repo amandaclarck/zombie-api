e20124ec465b6036c0779f6f986570c07d0997b6
Amanda Pedroso <amanda.lssc@gmail.com>
2019-01-26 01:12:14 -0200

    Generating migrations to Survivor and Resources

---
+++ db/migrate/20190126002402_create_survivors.rb
@@ -0,0 +1,15 @@
+class CreateSurvivors < ActiveRecord::Migration[5.1]
+  def change
+    create_table :survivors do |t|
+      t.string :name
+      t.integer :age
+      t.string :gender
+      t.boolean :infected, default: false
+      t.float :latitude
+      t.float :longitude
+      t.references :resource, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
