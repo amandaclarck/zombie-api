fdb3692defbce15e1993475cdcecb829c35759ca
Amanda Pedroso <amanda.lssc@gmail.com>
2019-01-26 01:10:51 -0200

    Spec folder

---
+++ spec/models/survivor_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Survivor, type: :model do
+	context '#validation' do
+		  it { should validate_presence_of(:name) }
+
+		  it { should validate_presence_of(:age) }
+
+		  it { should validate_presence_of(:gender) }
+
+		  context 'validates presence of location' do
+	  		it { should validate_presence_of(:latitude) }
+  			it { should validate_presence_of(:longitude) }
+		  end
+	end
+end
+
