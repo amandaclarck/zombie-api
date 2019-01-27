f2d71d1003c9371ef1f1c3d93f6f60003550eed0
Amanda Pedroso <amanda.lssc@gmail.com>
2019-01-27 00:55:58 -0200

    Changing context to describe

--- spec/models/survivor_spec.rb
+++ spec/models/survivor_spec.rb
@@ -2,16 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Survivor, type: :model do
 	context '#validation' do
-		  it { should validate_presence_of(:name) }
+	  it { should validate_presence_of(:name) }
 
-		  it { should validate_presence_of(:age) }
+	  it { should validate_presence_of(:age) }
 
-		  it { should validate_presence_of(:gender) }
+	  it { should validate_presence_of(:gender) }
 
-		  context 'validates presence of location' do
-	  		it { should validate_presence_of(:latitude) }
-  			it { should validate_presence_of(:longitude) }
-		  end
+	  describe 'validates presence of location' do
+  		it { should validate_presence_of(:latitude) }
+			it { should validate_presence_of(:longitude) }
+	  end
 	end
 end
 
