6bfd03ce492cee60731c6181d8c7fb9512e6c5e8
Amanda Pedroso <amanda.lssc@gmail.com>
2019-01-27 00:56:24 -0200

    Adding seed to Survivor

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,8 @@ Resource.create(name: 'Water', points: 4)
 Resource.create(name: 'Food', points: 3)
 Resource.create(name: 'Medication', points: 2)
 Resource.create(name: 'Ammunition', points: 1)
+Survivor.create(name: 'Teste', age: 25, gender: 'Male', infected: false, latitude: 124, longitude: 4545)
+
+
 
 
