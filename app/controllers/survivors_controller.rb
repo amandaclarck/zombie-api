848d93d56ef5ec0a380c2594f69ebe051e735a23
Amanda Pedroso <amanda.lssc@gmail.com>
2019-01-30 08:07:19 -0200

    Improving create method and creating update method. Adding survivor_params, survivor_update_params and survivor_id methods

--- app/controllers/survivors_controller.rb
+++ app/controllers/survivors_controller.rb
@@ -1,40 +1,62 @@
 class SurvivorsController < ApplicationController
 	def index
-#		@survivors = Survivor.first.resources
-
- #   format.json { render json: @survivors, status: :ok }
-
 	end
 
 	def show
-		#@survivor = Survivor.first
-		@survivors = Survivor.first.resources
-    #format.json { render json: @survivor, status: :ok }
-		render json: {status: 'SUCCESS', message:'Survivor(s) found!', data: @survivors},status: :ok
-
+		survivor_id
 	end
-=begin
+
 	def new
+		@survivor = Survivor.new(survivor_params)
 	end
 
 	def create
-    @survivor = Survivor.new(survivor_params)
+		@survivor = Survivor.new(survivor_params)
+    @survivor.resources << Resource.where(id: params[:resources].map { |r| r['id'] })
 
-    if @survivor.save
-			render json: {status: 'SUCCESS', message:'Survivor created!', data: @survivor},status: :ok
+		if @survivor.validate_gender || @survivor.validate_resources
+			render json: {status: 'ERROR', message: @survivor.errors}, status: :unprocessable_entity 
 		else
-			render json: {status: 'ERROR', message:'Survivor not created', data:@survivor.erros},status: :unprocessable_entity
-    end
-		
+			if @survivor.save
+				render json: {status: 'SUCCESS', message:'Survivor created!', data: @survivor}, status: :ok
+			else
+				render json: {status: 'ERROR', message:'Survivor not created', data:@survivor.errors}, status: :unprocessable_entity
+			end
+		end
 	end
 
-	def show
-		@survivor = Survivor.find(params[:id])
+	def update
+		survivor_id
+
+		if @survivor.update(survivor_params)
+			render json: {status: 'SUCCESS', message:'Survivor updated!', data: @survivor}, status: :ok
+		else
+			render json: {status: 'ERROR', message:'Survivor not updated', data:@survivor.errors}, status: :unprocessable_entity
+		end
+	end
+
+	def mark_as_infected
+    @survivor_ids << Survivor.where(id: params[:survivors].map { |s| s['id'] })
+    @survivor_infections = SurvivorInfection.where(id: params[:survivors].map { |s| s['id'] })
+
+    if @survivor_infections.size > 0
+		#pegar id do indicador que vai marcar o survivor 2 como infectado e o id do infectado
+		#verificar se já existe na tabela survivor_infection uma marcação para o id do survivor para o survivor 2
+		#se existir, mostrar erro que o mesmo indicador não pode fazer duas indicações
+		#se não existir, inserir o id do indicador e do survivor
 	end
 
 	private 
 	def survivor_params
+		params.require(:survivor).permit(:name, :age, :gender, :latitude, :longitude, resources: [])
+	end
+
+	def survivor_update_params
 		params.require(:survivor).permit(:name, :age, :gender, :latitude, :longitude)
 	end
-=end
+
+	def survivor_id
+		@survivor = Survivor.find(params[:id])
+	end
+
 end
