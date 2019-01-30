9e94d4a1d2b8afb249b6109d0825c28ee42cae98
Amanda Pedroso <amanda.lssc@gmail.com>
2019-01-30 08:10:00 -0200

    Creating Survivor Infection table

---
+++ db/migrate/20190129234713_create_survivor_infections.rb
@@ -0,0 +1,9 @@
+class CreateSurvivorInfections < ActiveRecord::Migration[5.1]
+  def change
+    create_table :survivor_infections do |t|
+      t.references :survivor_indicator
+      t.references :survivor_infected
+
+    end
+  end
+end
