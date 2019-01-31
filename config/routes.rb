dbce68c4326d2fc325aba687e3245d55537afd6a
Amanda Pedroso <amanda.lssc@gmail.com>
2019-01-31 00:38:45 -0200

    Adding route to mark a survivor as infected

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :survivors
   resources :resources
+
+  resources :survivors do
+    post 'mark_as_infected/:survivor_infected/:infected', action: :mark_as_infected, to: 'survivors#mark_as_infected'
+  end
 end
