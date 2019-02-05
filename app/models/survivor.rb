f78a13029f3bb881a71374a50c0e144ba23f256c
Amanda Pedroso <amanda.lssc@gmail.com>
2019-02-04 23:36:35 -0200

    Add update action to latitude and longitude and add create action to name, age, gender and resources

--- app/models/survivor.rb
+++ app/models/survivor.rb
@@ -4,12 +4,13 @@ class Survivor < ApplicationRecord
   has_and_belongs_to_many :resources
   has_many :survivor_indicator, class_name: 'Survivor', foreign_key: 'survivor_id'
   has_many :survivor_infected, class_name: 'Survivor', foreign_key: 'survivor_id'
+  accepts_nested_attributes_for :resources
 
-	validates :name, presence: true
-	validates :age, presence: true
-	validates :latitude, presence: true
-	validates :longitude, presence: true
+	validates :name, presence: true, on: :create
+	validates :age, presence: true, on: :create
+	validates :latitude, presence: true, on: :create, on: :update 
+	validates :longitude, presence: true, on:  :create, on: :update 
 	validates :infected, presence: true, on: :update
 	validates :gender, presence: true, inclusion: { in: %w(P M U), message: "Gender must be F, M or U" }, on: :create
-	validates :resources, presence: true
+	validates :resources, presence: true, on: :create
 end
