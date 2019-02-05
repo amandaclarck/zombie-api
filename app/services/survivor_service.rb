8114176305af763c17027d730cc9f50a62216c5d
Amanda Pedroso <amanda.lssc@gmail.com>
2019-02-04 23:37:11 -0200

    Commenting get resources logic to improve later

--- app/services/survivor_service.rb
+++ app/services/survivor_service.rb
@@ -2,6 +2,12 @@ class SurvivorService
 	class << self
 	  def create(survivor)
 	    @survivor = Survivor.new(survivor)
+=begin
+      @resources = Resource.where(id: @survivor.resources.map { |r| r.id })
+	    if @resources.length > 0
+			  @survivor.resources << @resources
+	    end
+=end
 	  end
 	end
 end
