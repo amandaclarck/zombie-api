f22d463d755c5c015daf5eb51fb2b1c8f54c3faf
Amanda Pedroso <amanda.lssc@gmail.com>
2019-02-06 08:14:23 -0200

    Refactoring the SurvivorInfection get logic and add errors array. Renaming survivor_infected to survivor

--- app/services/survivor_service.rb
+++ app/services/survivor_service.rb
@@ -11,20 +11,23 @@ class SurvivorService
 	  end
 
 	  def mark_as_infected(params)
-	    @survivor_indicator = Survivor.find(params[:survivor_id])
-	    @survivor_infected = Survivor.find(params[:id])
+	    @survivor_indicator = Survivor.find(params[:survivor_indicator])
+	    @survivor = Survivor.find(params[:id]) #infected
 
 	    @survivor_ids = []
 	    @survivor_ids << @survivor_indicator
-	    @survivor_ids << @survivor_infected
+	    @survivor_ids << @survivor
 
 	    @survivor_infections = []
-	    @survivor_infections = SurvivorInfection.where(survivor_indicator_id: @survivor_ids.map(&:id))
+	    @survivor_infections = SurvivorInfection.where(survivor_indicator_id: @survivor_ids[0].id, survivor_infected_id: @survivor_ids[1].id)
 
+	    @survivor_ids.each { |s| puts s.attributes}
     	if @survivor_infections.empty?
       	SurvivorInfection.create(survivor_indicator_id: @survivor_ids[0].id, survivor_infected_id: @survivor_ids[1].id)
+      else
+      	@survivor.errors.add(:id, message:'Can not be marked as infected by the same survivor')
     	end
-    	@survivor_infected
-    end
+    	@survivor
+  	end
 	end
 end
