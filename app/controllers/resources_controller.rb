5b429f80c7b2d907e1177b6f9eddc74dcccd95ca
Amanda Pedroso <amanda.lssc@gmail.com>
2019-02-06 08:11:02 -0200

    Add resources per survivor method

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -4,4 +4,8 @@ class ResourcesController < ApplicationController
 	def index
 		render json: { data: Resource.all }
 	end
+
+	def resources_per_survivor
+		render json: {data: Resource.joins(:survivors).where(id: params[:survivor_id])}
+	end
 end
