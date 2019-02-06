ea49dbca3983d64fdd1c438dca6fc1384667a4fe
Amanda Pedroso <amanda.lssc@gmail.com>
2019-02-05 22:15:09 -0200

    Extracting mark_as_infected method to survivor service and add params array to create method

--- app/services/survivor_service.rb
+++ app/services/survivor_service.rb
@@ -1,7 +1,7 @@
 class SurvivorService
 	class << self
-	  def create(survivor)
-	    @survivor = Survivor.new(survivor)
+	  def create(params)
+	    @survivor = Survivor.new(params)
 =begin
       @resources = Resource.where(id: @survivor.resources.map { |r| r.id })
 	    if @resources.length > 0
@@ -9,5 +9,22 @@ class SurvivorService
 	    end
 =end
 	  end
+
+	  def mark_as_infected(params)
+	    @survivor_indicator = Survivor.find(params[:survivor_id])
+	    @survivor_infected = Survivor.find(params[:id])
+
+	    @survivor_ids = []
+	    @survivor_ids << @survivor_indicator
+	    @survivor_ids << @survivor_infected
+
+	    @survivor_infections = []
+	    @survivor_infections = SurvivorInfection.where(survivor_indicator_id: @survivor_ids.map(&:id))
+
+    	if @survivor_infections.empty?
+      	SurvivorInfection.create(survivor_indicator_id: @survivor_ids[0].id, survivor_infected_id: @survivor_ids[1].id)
+    	end
+    	@survivor_infected
+    end
 	end
 end
