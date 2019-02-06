14d3b6682f19628216130b136efe29b4a7f86ef0
Amanda Pedroso <amanda.lssc@gmail.com>
2019-02-06 08:15:01 -0200

    Add resources per survivor route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :survivors
   resources :resources
   post 'mark_as_infected/', action: :mark_as_infected, to: 'survivors#mark_as_infected'
-
+  get	'resources_per_survivor/:survivor_id', action: :resources_per_survivor, to: 'resources#resources_per_survivor'
   resources :survivors do
   end
 end
