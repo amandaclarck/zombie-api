4fcf74037728b2918d480875ddf2f9ce6cc8f002
Amanda Pedroso <amanda.lssc@gmail.com>
2019-02-06 22:48:54 -0200

    Add route to exchange resources, add index route to survivors, resources and reports and improve mark_as_infected route

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :survivors
   resources :resources
-  post 'mark_as_infected/', action: :mark_as_infected, to: 'survivors#mark_as_infected'
+
+  #survivors
+  get 'index', action: :index, to: 'survivor#index'
+  post 'mark_as_infected', action: :mark_as_infected, to: 'survivors#mark_as_infected'
+  post 'exchange_resources', action: :exchange_resources, to: 'survivors#exchange_resources'
+
+  #resources
+  get 'resources', action: :index, to: 'resources#index'
   get	'resources_per_survivor/:survivor_id', action: :resources_per_survivor, to: 'resources#resources_per_survivor'
-  resources :survivors do
-  end
+
+  #reports
+  get 'reports', action: :index, to: 'reports#index'
 end
