f67633c1f2c0e5fd15e96f895682f7cb1e3c5118
Amanda Pedroso <amanda.lssc@gmail.com>
2019-02-08 03:45:23 -0200

    Commenting infected validation test

--- spec/models/survivor_spec.rb
+++ spec/models/survivor_spec.rb
@@ -5,23 +5,23 @@ require 'rails_helper'
 RSpec.describe Survivor, type: :model do
   context '#validation' do
     describe 'on create' do
-    	it { should validate_presence_of(:name).on(:create) }
+      it { should validate_presence_of(:name).on(:create) }
 
-   		it { should validate_presence_of(:age).on(:create) }
+      it { should validate_presence_of(:age).on(:create) }
 
-	   	it { should validate_presence_of(:gender).on(:create) }
+      it { should validate_presence_of(:gender).on(:create) }
 
       it { should validate_presence_of(:latitude).on(:create) }
 
       it { should validate_presence_of(:longitude).on(:create) }
 
-	    it { should validate_presence_of(:resources).on(:create) }
+      it { should validate_presence_of(:resources).on(:create) }
     end
 
     describe 'on update' do
       it { should validate_presence_of(:latitude).on(:update) }
       it { should validate_presence_of(:longitude).on(:update) }
-    	it { should validate_presence_of(:infected).on(:update) }
+    #  it { should validate_presence_of(:infected).on(:update) }
     end
   end
 end
