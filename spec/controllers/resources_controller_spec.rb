5e955f3c1af5bc08395bfc824b1fd47ced946ba4
Amanda Pedroso <amanda.lssc@gmail.com>
2019-02-08 03:44:39 -0200

    Rubocops fix

--- spec/controllers/resources_controller_spec.rb
+++ spec/controllers/resources_controller_spec.rb
@@ -3,12 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe ResourcesController, type: :controller do
-	context 'GET #index' do
-		before { get :index }
-		it { should respond_with(200) }
-	end
-	context 'GET #resources_per_survivor' do
-		before { get :resources_per_survivor }
-		it { should respond_with(200) }
-	end
+  context 'GET #index' do
+    before { get :index }
+    it { should respond_with(200) }
+  end
+  context 'GET #resources_per_survivor' do
+    before { get :resources_per_survivor }
+    it { should respond_with(200) }
+  end
 end
--- spec/controllers/survivors_controller_spec.rb
+++ spec/controllers/survivors_controller_spec.rb
@@ -1,25 +1,27 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
-#require 'factories'
+# require 'factories'
 
 RSpec.describe SurvivorsController, type: :controller do
-	context '#create' do
-		$resource = Resource.create!(name: 'Water', points: 1)
+  context '#create' do
+    $resource = Resource.create!(name: 'Water', points: 1)
 
-		it 'should create a new survivor' do
-			params = {
-                 survivor: {
-                   name: 'Teste',
-                   age: 13,
-                   gender: 'M',
-                   latitude: 31313123123.33333,
-                   longitude: 450.9,
-                   resources:  { id: $resource.id }
-                 }
-               }
+    it 'should create a new survivor' do
+      params = {
+        survivor: {
+          name: 'Teste',
+          age: 13,
+          gender: 'M',
+          latitude: 31_313_123_123.33333,
+          longitude: 450.9,
+          resources: { id: $resource.id }
+        }
+      }
 
-			should permit(:name, :age, :gender, :latitude, :longitude, resources: []).
-                for(:create, params: params).
-                on(:survivor)
+      should permit(:name, :age, :gender, :latitude, :longitude, resources: [])
+        .for(:create, params: params)
+        .on(:survivor)
     end
-	end
+  end
 end
