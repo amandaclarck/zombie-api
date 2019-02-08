ec73df0db1e610af70fda1455c57c5cdfda586c8
Amanda Pedroso <amanda.lssc@gmail.com>
2019-02-08 02:34:35 -0200

    Remove unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,18 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :survivors
-  resources :resources
-
-  #survivors
-  get 'index', action: :index, to: 'survivor#index'
+  # survivors
+  get 'index', action: :index, to: 'survivors#index'
+  get 'show/:id', action: :show, to: 'survivors#show'
+  post 'create', action: :create, to: 'survivors#create'
+  post 'update', action: :update, to: 'survivors#update'
   post 'mark_as_infected', action: :mark_as_infected, to: 'survivors#mark_as_infected'
   post 'exchange_resources', action: :exchange_resources, to: 'survivors#exchange_resources'
 
-  #resources
+  # resources
   get 'resources', action: :index, to: 'resources#index'
-  get	'resources_per_survivor/:survivor_id', action: :resources_per_survivor, to: 'resources#resources_per_survivor'
+  get 'resources_per_survivor', action: :resources_per_survivor, to: 'resources#resources_per_survivor'
 
-  #reports
+  # reports
   get 'reports', action: :index, to: 'reports#index'
 end
