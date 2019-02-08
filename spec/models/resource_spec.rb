754f34fc9f1db980f2f4f84dd743b40182e40b73
Amanda Pedroso <amanda.lssc@gmail.com>
2019-02-08 02:36:58 -0200

    Add relationship test

--- spec/models/resource_spec.rb
+++ spec/models/resource_spec.rb
@@ -3,5 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe Resource, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context '#relationship' do
+    it { should have_and_belong_to_many(:survivors) }
+  end
 end
