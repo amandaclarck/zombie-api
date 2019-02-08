2f5a5015aa0ba4f837a967e74a1abca52850a2ea
Amanda Pedroso <amanda.lssc@gmail.com>
2019-02-08 02:36:32 -0200

    Add create test for survivor

--- spec/controllers/survivors_controller_spec.rb
+++ spec/controllers/survivors_controller_spec.rb
@@ -1,36 +1,25 @@
 require 'rails_helper'
+#require 'factories'
 
 RSpec.describe SurvivorsController, type: :controller do
-=begin
-	context '#new' do
-		setup do
-    		create(:resource, id: 1)
-  		end
-	    resource = { id: 1, resource: { activated: true } }
-		describe 'success' do
+	context '#create' do
+		$resource = Resource.create!(name: 'Water', points: 1)
 
+		it 'should create a new survivor' do
+			params = {
+                 survivor: {
+                   name: 'Teste',
+                   age: 13,
+                   gender: 'M',
+                   latitude: 31313123123.33333,
+                   longitude: 450.9,
+                   resources:  { id: $resource.id }
+                 }
+               }
 
-			it 'creates a new survivor' do
-		    params = {
-		      survivor: {
-		        name: 'Code',
-		        age: '12',
-		        gender: 'F',
-		        latitude: 123,
-		        longitude: 456,
-		        infected: false,
-						resources: resource
-		      }
-		    }
-		    should permit(:name, :age, :gender, :latitude, :longitude, :infected, resources: resource).
-		      for(:create, params: params).
-		      on(:survivor)
-			end
-		end
-
-
-		describe 'failure' do
-		end
+			should permit(:name, :age, :gender, :latitude, :longitude, resources: []).
+                for(:create, params: params).
+                on(:survivor)
+    end
 	end
 end
-=end
